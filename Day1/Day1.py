89ca2eb15651728dddff872ba82dee06a6123bad
Harvey <32651842+HarveyWilson@users.noreply.github.com>
2020-12-01 17:22:25 +0000

    Print out the found answers

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -6,6 +6,8 @@ data = list(map(int,data))
 while len(data)>3:
     a = data.pop()
     for b in data:
+        if a + b == 2020:
+            print(f'Puzzle 1: {a}, {b}. Answer is {a*b}')
         for c in data:
             if a + b + c == 2020:
-                print(a,b,c)
+                print(f'Puzzle 2: {a}, {b}, {c}. Answer is {a*b*c}')
