7d934f3c1ecffec51a8af4fe774d28e39d671f7d
Harvey <32651842+HarveyWilson@users.noreply.github.com>
2020-12-02 15:42:59 +0000

    Display solution for puzzle 1 and 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,10 +1,10 @@
 
 def parse_range(q):
     a,b = q.split('-')
-    return (int(a),int(b))
+    return int(a),int(b)+1
 
 def valid_pass(positions, password, letter):    
-    return (password[positions[0]-1] == letter) != (password[positions[1]-1] == letter)
+    return (password[positions[0]-1] == letter) != (password[positions[1]-2] == letter)
     
 
 with open('Day 2\\input.txt') as f:
@@ -13,8 +13,8 @@ with open('Day 2\\input.txt') as f:
 #{range, letter, password}
 data = [i.strip('\n').split(',') for i in data]
 
-data = [password for q,letter,password in data if valid_pass(parse_range(q),password,letter)]
+puzzle2 = [password for q,letter,password in data if valid_pass(parse_range(q),password,letter)]
 
-# data = [password for q,letter,password in data if password.count(letter) in parse_range(q)]
+puzzle1 = [password for q,letter,password in data if password.count(letter) in range(*parse_range(q))]
 
-print(len(data))
+print(f'Puzzle 1: {len(puzzle1)}\nPuzzle 2: {len(puzzle2)}')
