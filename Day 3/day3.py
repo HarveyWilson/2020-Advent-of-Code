a693f93b1e89b6f6a1dbbc5ca3b4abe1de20d96c
Harvey <32651842+HarveyWilson@users.noreply.github.com>
2020-12-03 16:33:35 +0000

    Solution to puzzle 2

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,10 +1,21 @@
+from functools import reduce
+
+def get_trees(slope, movement):
+    column = 0
+    count = 0
+    length = len(slope[0])-1
+    for i in range(0,len(data),movement[1]):
+        if slope[i][column] =='#':
+            count +=1
+        column = (column + movement[0]) % length
+    return count
+
 with open('Day 3\\input.txt') as f:
     data = f.readlines()
 
-column = 0
-count = 0
-for row in data:
-    if row[column] == '#':
-        count += 1
-    column = (column + 3) % 31
-print(count)
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+ans = []
+for slope in slopes:
+    ans.append(get_trees(data,slope))
+
+print(reduce((lambda x,y: x*y),ans))
