b838763567c087d5be3a61adfd89632b69d2c28b
Harvey <32651842+HarveyWilson@users.noreply.github.com>
2021-02-15 02:57:57 +0000

    Solution Day 6 Puzzle 2

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,19 +1,21 @@
-import collections
+from collections import Counter
+from itertools import groupby
 
 with open('Day 6\\input.txt') as f:
     data = f.readlines()
 
 data = [data.strip() for data in data]
 
-c = collections.Counter()
-num_yes = 0
-for form in data:
-    if not form:
-        num_yes += len(c.values())
-        c = collections.Counter()
-    else:
-        for letter in form:
-            c.update(letter)
+group_responses = [list(g) for k, g in groupby(data,key=bool) if k]
 
-num_yes += len(c.values())
-print(num_yes, len(c.values()))
+c = Counter()
+num_yes = 0
+for group in group_responses:
+    group_size = len(group)
+    c = Counter()
+    responses = ''.join(group)
+    for letter in responses:
+        c.update(letter)
+    c = Counter({k:c for k,c in c.items() if c>=group_size})
+    num_yes += len(c.values())
+print(num_yes)
