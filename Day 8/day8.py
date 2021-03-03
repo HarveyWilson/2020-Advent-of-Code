367e4f84f29d7030a9a867bd969bf7ee550b96b6
Harvey <32651842+HarveyWilson@users.noreply.github.com>
2021-03-03 01:45:57 +0000

    Solution to Day 8 puzzle 2

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -2,22 +2,34 @@ with open('Day 8\\input.txt') as f:
     data = f.readlines()
 
 instructions = [d.strip() for d in data]
-acc = 0
-step = 0
-steps = []
 
-while step not in steps:
-    steps.append(step)
-    cmd = instructions[step][:3]
-    val = int(instructions[step][3:])
-    if cmd == "nop":
-        step += 1
+changed_instructions = []
+
+for i,instruct in enumerate(instructions):
+    tmp_instruc = instructions.copy()
+    if tmp_instruc[i][:3] == "nop":
+        tmp_instruc[i] = "jmp" + instructions[i][3:]
+        changed_instructions.append(tmp_instruc)
         continue
-    if cmd == "acc":
-        acc += val
-        step += 1
-        continue
-    if cmd == "jmp":
-        step += val
-        continue
-print(step, acc)
+    if tmp_instruc[i][:3] == "jmp":
+        tmp_instruc[i] = "nop" + instructions[i][3:]
+        changed_instructions.append(tmp_instruc)
+
+for instruct in changed_instructions:
+    acc = 0
+    step = 0
+    steps = []
+    while step not in steps:
+        if step >= 638:
+            print(step, acc)
+            break
+        steps.append(step)
+        cmd = instruct[step][:3]
+        val = int(instruct[step][3:])
+        if cmd == "nop":
+            step += 1
+        if cmd == "acc":
+            acc += val
+            step += 1
+        if cmd == "jmp":
+            step += val
