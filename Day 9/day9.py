149390a6d328a5ba4221fdcd08d56c10802a3df0
Harvey <32651842+HarveyWilson@users.noreply.github.com>
2021-03-04 15:32:32 +0000

    Solution to Day 9 puzzle 2

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -3,7 +3,8 @@ with open('Day 9\\input.txt') as f:
     for line in f.readlines():
         data.append(int(line.strip()))
 
-for i in range(0,len(data)-25):
+weakness = None
+for i in range(0, len(data)-25):
     window = data[i:i+25]
     values = window.copy()
     results = []
@@ -11,4 +12,22 @@ for i in range(0,len(data)-25):
     for value in window:
         results.extend([x + value for x in values])
     if result not in results:
-        print(result)
+        weakness = result
+        break
+print(weakness)
+
+r = None
+for i in range(len(data)-1):
+    sum = 0
+    j = i+1
+    while sum < weakness:
+        sum += data[j]
+        j += 1
+        if sum == weakness:
+            r = (i, j)
+            break
+    if r is not None:
+        break
+
+print(min(data[r[0]:r[1]+1])+max(data[r[0]:r[1]+1]))
+print(r)
